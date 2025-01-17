2d46c62f5a2504b9b1df1ca89792089fb34c05f7
Mateusz Młynarski <kontakt@mlynarskimateusz.pl>
2025-01-17 16:08:04 +0100

    dodanie nowych funkcji, znajdz ludzi w pobliżu, poprawienie kilku błędów

---
+++ Rules/Features/People/Views/NearbyPeopleListView.swift
@@ -0,0 +1,7 @@
+//
+//  NearbyPeopleListVie.swift
+//  Rules
+//
+//  Created by Mateusz Mlynarski on 17/01/2025.
+//
+
---
+++ Rules/ViewControllers/Rules/SavedRulesViewController.swift
@@ -0,0 +1,7 @@
+//
+//  SavedRulesViewController.swift.swift
+//  Rules
+//
+//  Created by Mateusz Mlynarski on 04/01/2025.
+//
+
