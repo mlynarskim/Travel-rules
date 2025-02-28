7d28d3dea7ee97231faa794706780492581a115e
Mateusz Młynarski <kontakt@mlynarskimateusz.pl>
2025-02-28 10:39:27 +0100

    usuniecie podwójnego tła w my rules

--- Rules/ViewControllers/Rules/info.swift
+++
@@ -1,9 +0,0 @@
-//
-//  info.swift
-//  Rules
-//
-//  Created by Mateusz Mlynarski on 06/02/2025.
-//
-
-//plik w tym folderze odpowiadaja za wyswietlanie zapisanych zasad
-//jest mozliwosc dodania wlasnej zasady i usuniecia jej
